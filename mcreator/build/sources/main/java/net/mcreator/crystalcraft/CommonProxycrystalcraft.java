0555d54f37c70f8ae8ffcfb705e0b1087fa4ce0c
InferusAnima <InferusAnima@mail.ru>
2019-09-11 20:45:18 +0300

    deleted mod, added mcreator mod

---
+++ mcreator/build/sources/main/java/net/mcreator/crystalcraft/CommonProxycrystalcraft.java
@@ -0,0 +1,13 @@
+package net.mcreator.crystalcraft;
+
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class CommonProxycrystalcraft {
+
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	public void init(FMLInitializationEvent event) {
+	}
+}
