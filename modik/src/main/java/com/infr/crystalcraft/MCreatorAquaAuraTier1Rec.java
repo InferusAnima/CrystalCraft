29e4f6276583c81206eb4783d619f8a3132411af
InferusAnima <InferusAnima@mail.ru>
2019-09-16 18:08:33 +0300

    added JEI

---
+++ modik/src/main/java/com/infr/crystalcraft/MCreatorAquaAuraTier1Rec.java
@@ -0,0 +1,8 @@
+package com.infr.crystalcraft;
+
+public class MCreatorAquaAuraTier1Rec extends crystalcraft.ModElement {
+
+	public MCreatorAquaAuraTier1Rec(crystalcraft instance) {
+		super(instance);
+	}
+}
