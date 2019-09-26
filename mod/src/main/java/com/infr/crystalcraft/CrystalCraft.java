a927e5a8300c8c8e287f663d7f32351270bc8d05
InferusAnima <InferusAnima@mail.ru>
2019-09-26 16:23:26 +0000

    updated mod

--- mod/src/main/java/com/infr/crystalcraft/CrystalCraft.java
+++ mod/src/main/java/com/infr/crystalcraft/CrystalCraft.java
@@ -1,20 +1,22 @@
-package com.example.examplemod;
+package com.infr.crystalcraft;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = CrystalCraft.MODID, name = CrystalCraft.NAME, version = CrystalCraft.VERSION)
+public class CrystalCraft
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "crystalcraft";
+    public static final String NAME = "Crystal Craft";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
+
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -26,6 +28,6 @@ public class ExampleMod
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getUnlocalizedName());
     }
 }
